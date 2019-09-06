cfcbab03295af0ed4a9b5fb8e4d83c514c4f52c4
Allan Cruz <allancruzrosa@gmail.com>
2019-09-06 14:33:11 -0300

    Desabilitar menu na pagina inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 
 @IonicPage()//permite eu referenciar a classe por String exemplo: "HomePage".
@@ -10,12 +11,22 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController , public menu: MenuController) {
   }
+
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+    }
+    
+    ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+    }
 
   public login(){
     this.navCtrl.setRoot('CategoriasPage');
         //push empilha as paginas - this.navCtrl.push('CategoriasPage');
   }
+
+  
 
 }
