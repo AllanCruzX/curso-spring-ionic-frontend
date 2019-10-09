e006abdd60d2c57eb2ebe8ceacf6cb6102a87a97
allanCruzX <allancruzrosa@gmail.com>
2019-10-08 23:29:32 -0300

    Obtendo imagem da galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "https://curso-spring-ionic-j.herokuapp.com/",
+    baseUrl: "https://curso-spring-ionic-j.herokuapp.com",
     bucketBaseUrl: "https://allan-curso-spring-ionic.s3-sa-east-1.amazonaws.com"
 
 
